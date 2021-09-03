d429a475674efe60372c43d2b0c17036ab6a833f
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-09-03 17:57:03 -0300

    Correction BithDate to BirthDate

--- SellerWebMvc/Migrations/20210903124318_OtherEntities.cs
+++ SellerWebMvc/Migrations/20210903124318_OtherEntities.cs
@@ -16,7 +16,7 @@ namespace SaleWebMvc.Migrations
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
                     Name = table.Column<string>(nullable: true),
                     Email = table.Column<string>(nullable: true),
-                    BithDate = table.Column<DateTime>(nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false),
                     BaseSalary = table.Column<double>(nullable: false),
                     DepartmentId = table.Column<int>(nullable: true)
                 },
