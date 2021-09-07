07c427fff99621a0e5048e618c0cc53d25fc66c9
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-09-07 11:10:40 -0300

    Seeding Service

--- SellerWebMvc/Startup.cs
+++ SellerWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SaleWebMvc.Models;
+using SaleWebMvc.Data;
 
 namespace SellerWebMvc
 {
@@ -38,14 +39,17 @@ namespace SellerWebMvc
 
             services.AddDbContext<SaleWebMvcContext>(options =>
                    options.UseMySql(Configuration.GetConnectionString("SaleWebMvcContext"), builder => builder.MigrationsAssembly("SaleWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
