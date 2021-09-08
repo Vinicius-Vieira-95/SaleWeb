4526550d49d5b4e7bdb2df17d88ba30f501a7ec7
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-09-08 11:17:21 -0300

    Foreign Key not null

--- SellerWebMvc/Models/Seller.cs
+++ SellerWebMvc/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SaleWebMvc.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> sales { get; set; } = new List<SalesRecord>();
 
 
