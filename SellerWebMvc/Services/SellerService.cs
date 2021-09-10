07216771c578ebf9c2ab3affcc68eee3ce1fb60c
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-09-10 14:51:34 -0300

    Update seller and custom service exception

--- SellerWebMvc/Services/SellerService.cs
+++ SellerWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SaleWebMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using SaleWebMvc.Services.Exceptions;
 
 namespace SaleWebMvc.Services
 {
@@ -39,6 +40,22 @@ namespace SaleWebMvc.Services
             _context.SaveChanges();
         }
 
+        public void Update( Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
 
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
     }
 }
