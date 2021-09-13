1e5c462f5e96c34d6c155880946dac36c1a9a0a4
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-09-13 09:49:57 -0300

    Exception for delete (reference integryty)

---
+++ SellerWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
